8ff5297ead803371861d8e168a14d964b8d7d19e
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-03-20 14:49:23 +0300

    .

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -1,6 +1,38 @@
-public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordFrequency {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please write a text:");
+        String text = input.nextLine();
+
+        // Split text into words
+        String[] arrayText = text.split(" ");
+
+        //Creating a HashMap to store words
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        for (String word : arrayText) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+            System.out.println(wordCountMap);
+        }
+
+        // Finding most frequent word
+        int maxFrequency = 0;
+        String mostMentionedWord = null;
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+                mostMentionedWord = entry.getKey();
+            }
+        }
+        // Printing result
+        System.out.println("Most frequent word: " + mostMentionedWord + " Count: " + maxFrequency);
     }
 }
